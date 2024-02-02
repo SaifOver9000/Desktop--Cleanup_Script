6de2d38521f469e6cee73c1e4bd9c187ca5d520c
SaifOver9000 <123909641+SaifOver9000@users.noreply.github.com>
2024-02-02 18:58:44 -0400

    added some function that returns the enteries in the directiry we are trying to organize.

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-import os
 import sys
-import json
-import shutil
+import time
+import logging
+import os
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
+origin_dir = "C:\\Users\\Saif\\OneDrive\\Desktop"
 
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with os.scandir(origin_dir) as enteries:
+            for entry in enteries:
+                name = entry.name
+                #dest = 
+    
+    
 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = origin_dir
+    event_handler = MoveHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
