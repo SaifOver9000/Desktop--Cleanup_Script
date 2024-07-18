825bd529d99802ea039eb00ae61bf36edb78f4a2
M_Saif_M <123909641+SaifOver9000@users.noreply.github.com>
2024-07-18 17:07:32 +0000

    Planning Stage

--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-import sys
-import time
-import logging
 import os
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
 
-origin_dir = "C:\\Users\\Saif\\OneDrive\\Desktop"
-
-class MoverHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        with os.scandir(origin_dir) as enteries:
-            for entry in enteries:
-                name = entry.name
-                #dest = 
-    
-    
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = origin_dir
-    event_handler = MoveHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+#TODO: Build a Script that checks if there are files in the Directory
