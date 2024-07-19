0935fbf70ea49a043eeca3ed6b3cd658a8a86bbf
M_Saif_M <123909641+SaifOver9000@users.noreply.github.com>
2024-07-18 21:28:32 -0400

    Added The list of common file extensions

--- main.py
+++ main.py
@@ -10,9 +10,32 @@ def file_exist(path):
     else:
         return False
 #TODO: Build a Script that Moves files from One directory to another,
-# I need to build a list of the potential file types and create a folder based on them.
-
-#TODO: Build a Script that Lists out the type of files in the directory
+# I need to build a list of the potential file types and create a folder based on them if they exist in the directory.
+#divide the file into "." and organize them into it
+common_file_extensions = [
+    # Document files
+    '.doc', '.docx', '.pdf', '.txt', '.odt', '.rtf',
+    # Spreadsheet files
+    '.xls', '.xlsx', '.csv', '.ods',
+    # Presentation files
+    '.ppt', '.pptx', '.odp',
+    # Image files
+    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg',
+    # Audio files
+    '.mp3', '.wav', '.aac', '.flac', '.ogg',
+    # Video files
+    '.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm',
+    # Compressed files
+    '.zip', '.rar', '.tar', '.gz', '.7z',
+    # Code files
+    '.py', '.java', '.cpp', '.c', '.js', '.html', '.css', '.php', '.rb',
+    # Executable files
+    '.exe', '.bat', '.sh',
+    # System files
+    '.dll', '.sys', '.ini', '.log',
+    # Database files
+    '.db', '.sql', '.mdb', '.accdb'
+]
 #TODO: Create a folder for each file type
 #TODO: Move File into Associated folder, DONT touch folders.
 
