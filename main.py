e6ee97aa99d84543c79bd84d5a11fae9a49407f1
SaifOver9000 <123909641+SaifOver9000@users.noreply.github.com>
2024-12-07 22:56:35 -0400

    Created a gitignorefile as well as started work back on this script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 #TODO: Build a function that checks if there are files in a directory, Returns True if files are present
 
@@ -14,6 +15,13 @@ def fileCheck():
         return True
 #TODO: Create a function that detects the types of files in the directory
 
+def file_TypeCheck(filetype):
+    p = Path('C:\\Users\\Saif\\OneDrive\\Desktop')
+    
+    files = [x for x in p.iterdir() if x.suffix == f".{filetype}" ] #example text file type
+    
+    for file in files:
+        print(file)
 
 #TODO: Buid a function that moves files from one directory to another.
 
@@ -22,3 +30,6 @@ def fileCheck():
 #TODO: Dont touch the folders in the current directory.
 
 #TODO: Make the Program run in the background
+
+#TODO: Upload this to the repo
+file_TypeCheck("docx")
