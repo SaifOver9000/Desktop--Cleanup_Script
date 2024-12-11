2de4035161346ab54ac22e5aec56ef13c0afb9c9
SaifOver9000 <123909641+SaifOver9000@users.noreply.github.com>
2024-12-11 18:33:26 -0400

    Added some commented code that makes the code run in the background. need to introduce the watchdog module to make the script run in the background.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-import os
+import sched
+import time
 import shutil
 
 
@@ -75,12 +76,10 @@ folder_names = {
     '.accdb': "Database"
     }
 
-#TODO: Build a function that checks if there are files in a directory, Returns True if files are present
-
 #TODO: Create a function that detects the types of files in the directory
 
-def file_TypeCheck():
-    p = Path('C:\\Users\\Saif\\OneDrive\\Desktop')
+def file_TypeCheck(user_dict_path):
+    p = Path(user_dict_path) #make this more dynamic ---'C:\\Users\\Saif\\OneDrive\\Desktop'
     
     #files = [x for x in p.iterdir() if x.suffix == f".{filetype}" ] #Here it prints out the specific file type you want to see
     files = [x for x in p.iterdir()]
@@ -95,7 +94,7 @@ def move_files(user_dict_path):
     for file in p.iterdir():
         if file.is_file():
             file_ext = file.suffix
-            foldertype = folder_names[file_ext]
+            foldertype = folder_names.get(file_ext, "Others")
             destinationpath = p / foldertype
             if (destinationpath.exists()):
                 shutil.move(file, destinationpath)
@@ -107,17 +106,23 @@ def move_files(user_dict_path):
         #if not we would then create the folder, based on the foldertype assigned 
         
         
-    
-    
-
-#TODO: Dont touch the folders in the current directory.
-
-#TODO: Make the Program run in the background --- Automation
-
-#TODO: Upload this to the repo
-
 user_dict_path = input("Please enter the  path of the directory you want to organise: ")
 
 #print(folder_names[0])
 move_files(user_dict_path)
+
+
+#TODO: Make the Program run in the background --- Automation
+
+#Debug and see how this works. make sure that the folders that you want organise exist.
+#TODO: ADD the functionality of like organising according to date created, alphabetical.. etc.
+
+
+# event_Scheduler = sched.scheduler(time.time, time.sleep)
+
+# event_Scheduler.enter(30, 1, move_files,(user_dict_path,)) #We added a comma after the arguement because the enter function expects a tuple
+
+#TODO: Upload this to the repo
+
+
     
